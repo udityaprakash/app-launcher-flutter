c3e6a08fffa8496cc8e7aa413432b0e0aee3c1f3
udityaprakash <udityaprakash983@gmail.com>
2024-03-16 00:46:49 +0530

    created ui for app launcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:device_apps/device_apps.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,46 +11,136 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const MyHomePage(title: 'WATERLESS'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'code'),
+      home: waterlesshomepage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
+class waterlesshomepage extends StatefulWidget {
+  const waterlesshomepage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<waterlesshomepage> createState() => _waterlesshomepageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+class _waterlesshomepageState extends State<waterlesshomepage> {
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 44, 47, 54),
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        title: Text(
+          'WATERLESS',
+          style: TextStyle(
+            color: const Color.fromARGB(255, 252, 252, 252),
+            fontSize: 20,
+            fontFamily: 'code',
+            letterSpacing: 5.0,
+            // wordSpacing: 0.1,
+          ),
+        ),
         centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: <Widget>[],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await input(context, controller);
+          
+        },
+        child: const Icon(
+          Icons.terminal,
+          color: Colors.white,
+          size: 30,
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
       ),
     );
   }
+}
+
+Future<dynamic> input(BuildContext context, TextEditingController controller) {
+  controller.clear();
+  return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+            shadowColor: Color.fromARGB(255, 44, 47, 54),
+            child: Container(
+            decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Color.fromARGB(255, 44, 47, 54),
+            ),
+            height: 200,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    child: TextField(
+                      controller: controller,                    
+                      decoration: InputDecoration(
+                        helperStyle: TextStyle(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          fontFamily: 'code'),
+                         border: InputBorder.none,
+                          hintText: 'Promt here...',
+                          hintStyle: TextStyle(
+                            color: const Color.fromARGB(185, 255, 255, 255),
+                            fontFamily: 'code',
+                          ),
+                          
+                          prefixIcon: Icon(Icons.arrow_forward_ios,
+                          color: const Color.fromARGB(185, 255, 255, 255),
+                          size: 30,
+                          ),
+                        ),
+                        
+                        cursorColor: Colors.transparent,
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          // backgroundColor: const Color.fromARGB(255, 44, 47, 54),
+                          
+                        ),
+                        cursorWidth: 2.0,
+                        cursorRadius: Radius.circular(1.0),
+                        showCursor: true,
+                        autofocus: true,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.keyboard_return,
+                      // Icons.keyboard_option_key,
+                      color: const Color.fromARGB(185, 255, 255, 255),
+                      size: 30,
+                    ),
+                    // color: const Color(0xFF1BC0C5),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      });
 }
